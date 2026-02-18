8a64c20403938265c473f7dd132b2e8cbaab176c
YonuelTutorial <yonuel01@hotmail.com>
2026-02-18 15:40:27 -0400

    final Add UI text objects and hook to player controller
    Add a Canvas, two UI Text elements (TextoContador, TextoGanar) and an EventSystem to MainEscena.unity and connect them to the JugadorController instance. Update JugadorController.cs: rename public Text fields to TextoContador/TextoGanar, initialize UI text in Start(), add comments, and update SetTextoContador() to display the counter and show the victory message when contador >= 12. Also apply small prefab position modifications in the scene.

--- Assets/Scripts/JugadorController.cs
+++ Assets/Scripts/JugadorController.cs
@@ -4,41 +4,45 @@ using UnityEngine;
 using UnityEngine.UI; 
 
 public class JugadorController : MonoBehaviour {
+
     public float velocidad;
     private Rigidbody rb;
-    
+
+    // Variables para los textos y el conteo
     private int contador;
-    public Text textoContador;
-    public Text textoGanar;
+    public Text TextoContador;
+    public Text TextoGanar;
 
     void Start () {
         rb = GetComponent<Rigidbody>();
-        contador = 0; 
-        SetTextoContador(); 
-        textoGanar.text = "";
+        contador = 0;
+        
+        // Inicializamos los textos al arrancar el juego
+        SetTextoContador();
+        TextoGanar.text = ""; // Lo dejamos vacío al inicio [cite: 509]
     }
 
     void FixedUpdate () {
         float movimientoH = Input.GetAxis("Horizontal");
         float movimientoV = Input.GetAxis("Vertical");
-        
+
         Vector3 movimiento = new Vector3(movimientoH, 0.0f, movimientoV);
-        
         rb.AddForce(movimiento * velocidad);
     }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Coleccionable")) {
             other.gameObject.SetActive(false);
-            contador = contador + 1;
-            SetTextoContador();
+            contador = contador + 1; // Sumamos 1 al contador
+            SetTextoContador(); // Actualizamos el texto en pantalla
         }
     }
 
+    // Función personalizada para actualizar la UI
     void SetTextoContador() {
-        textoContador.text = "Contador: " + contador.ToString();
+        TextoContador.text = "Contador: " + contador.ToString();
         if (contador >= 12) {
-            textoGanar.text = "¡Ganaste!";
+            TextoGanar.text = "¡Ganaste!";
         }
     }
 }
